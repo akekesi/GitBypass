adb5da397fee0dbefaabe97eb860fd565b9943f2
akekesi <kekesi.att@gmail.com>
2023-11-21 22:58:16 +0100

    refactored

--- 01_git/original/bypass.sh
+++ 01_git/original/bypass.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 name_copy_dir=original                  # name of the folder to copy
-name_cloned_git="bypass_git"            # name of rhe cloned git repository
+name_cloned_git=bypass_git              # name of rhe cloned git repository
 path_bypass_y=$name_cloned_git/bypass_y # relative path of bypass folder with flag y: yes, use bypass
 path_bypass_n=$name_cloned_git/bypass_n # relative path of bypass folder with flag n: no, do not use bypass
 git_ssh=git@github.com:akekesi/GitBypass-Git.git
-
 
 if [ ! -d $name_cloned_git ]; then
     git clone $git_ssh $name_cloned_git > /dev/null 2>&1
